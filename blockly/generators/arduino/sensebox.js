a821730107e6cefe1bbdec538187a64d8cb257d5
Mario <mario.pesch@uni-muenster.de>
2019-10-23 11:23:56 -0300

    fix BME680 :fire:
    setGasHeater to 0

--- blockly/generators/arduino/sensebox.js
+++ blockly/generators/arduino/sensebox.js
@@ -43,6 +43,7 @@ Blockly.Arduino.sensebox_sensor_bme680 = function() {
   Blockly.Arduino.setups_['bme_pressure_oversampling'] = 'bme.setPressureOversampling(BME680_OS_4X);';
   Blockly.Arduino.setups_['bme_setIIR'] = 'bme.setIIRFilterSize(BME680_FILTER_SIZE_3);';
   Blockly.Arduino.loops_['bme_performReading'] = 'bme.performReading();';
+  Blockly.Arduino.setups_['bme_gas_heater'] = 'bme.setGasHeater(0,0);'; 
     var code ='bme.' + dropdown_name;
     if (dropdown_name == 'gas_resistance' && dropdown_name != 'temperature' && dropdown_name != 'humidity' && dropdown_name != 'pressure')
     {
